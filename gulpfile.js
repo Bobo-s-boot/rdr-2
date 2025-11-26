facf8855adbb0db42e6d8eac439f412dd7ad4056
Шевченко Богдан <bohdansheva06@gmail.com>
2025-11-26 18:59:38 +0200

    fix docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,28 +10,40 @@ const paths = {
 };
 
 function minifyCSS() {
-  return gulp.src(paths.css).pipe(cleanCSS()).pipe(gulp.dest("dist"));
+  return gulp
+    .src(paths.css)
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("docs"))
+    .pipe(browserSync.stream());
 }
 
 function minifyJS() {
-  return gulp.src(paths.js).pipe(uglify()).pipe(gulp.dest("dist"));
+  return gulp
+    .src(paths.js)
+    .pipe(uglify())
+    .pipe(gulp.dest("docs"))
+    .pipe(browserSync.stream());
 }
 
 function copyHTML() {
-  return gulp.src(paths.html).pipe(gulp.dest("dist"));
+  return gulp
+    .src(paths.html)
+    .pipe(gulp.dest("docs"))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "dist",
+      baseDir: "docs",
     },
   });
 
-  gulp.watch(paths.css, minifyCSS).on("change", browserSync.reload);
-  gulp.watch(paths.js, minifyJS).on("change", browserSync.reload);
-  gulp.watch(paths.html, copyHTML).on("change", browserSync.reload);
+  gulp.watch(paths.css, minifyCSS);
+  gulp.watch(paths.js, minifyJS);
+  gulp.watch(paths.html, copyHTML);
 }
 
 gulp.task("default", gulp.series(minifyCSS, minifyJS, copyHTML, watch));
+
 gulp.task("build", gulp.series(minifyCSS, minifyJS, copyHTML));
