ce63e2c6c65f7329d67e74278da3ccd3bc6a7968
Шевченко Богдан <bohdansheva06@gmail.com>
2025-12-01 14:52:08 +0200

    finish

--- src/script.js
+++ src/script.js
@@ -30,4 +30,28 @@ setInterval(() => {
   nextSlide();
 }, 8000);
 
+const shotgunSection = document.querySelector(".shotgun-section");
+const shell1 = document.querySelector(".shell-1");
+const shell2 = document.querySelector(".shell-2");
+const flash = document.querySelector(".muzzle-flash");
+let hasFired = false;
+
+window.addEventListener("scroll", () => {
+  const position = shotgunSection.getBoundingClientRect();
+
+  // Перевірка, чи секція в межах видимої області
+  if (position.top < window.innerHeight && position.bottom >= 0 && !hasFired) {
+    hasFired = true;
+
+    // Запуск спалаху
+    flash.classList.add("flash-animation");
+
+    // Запуск гільз із невеликою затримкою
+    setTimeout(() => {
+      shell1.classList.add("drop-animation-1");
+      shell2.classList.add("drop-animation-2");
+    }, 500);
+  }
+});
+
 window.addEventListener("resize", updateSlide);
